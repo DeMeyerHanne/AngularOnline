08f6abc2fe10d9ba1b8a99dddf22fa7491756b0a
DeMeyerHanne <hanne.de.meyer@student.howest.be>
2021-10-09 12:45:04 +0200

    2. Display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes', // Components CSS element selector
   templateUrl: './heroes.component.html', // Location of the component's template file
@@ -8,13 +10,15 @@ import { Hero } from '../hero';
 
 // Always exportt the class so you can import it elsewhere. Ex. in AppModule
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() {}
 
   // Wordt gecalled nadat component aangemaakt is. Goed voor initialization logic.
   ngOnInit() {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
